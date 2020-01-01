3328af7805e720541ca44fe2a80e4339301387f1
BRobinett <brobinett11@gmail.com>
2020-01-01 14:34:08 -0600

    Added content container for resources

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Desc from "./Desc";
+import Resources from "./Resources";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Desc />
+        <Resources />
       </div>
     );
   }
---
+++ src/Resources.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Resources extends Component {
+  render() {
+    return <div style={styles.container} />;
+  }
+}
+
+const styles = {
+  container: {
+    border: "1px solid red",
+    padding: "10px"
+  }
+};
